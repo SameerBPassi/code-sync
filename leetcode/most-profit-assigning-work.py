03b192ca33b67d4b60406d776af8999635f07a5e
Sameer Passi <sameerbpassi@gmail.com>
2025-10-25 15:16:51 -0400

    Add solution for most-profit-assigning-work

---
+++ leetcode/most-profit-assigning-work.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        jobs = sorted(zip(difficulty, profit))
+        worker.sort()
+
+        total_profit = 0
+        max_profit = 0
+        i = 0
+
+        for ability in worker:
+            while i < len(jobs) and jobs[i][0] <= ability:
+                max_profit = max(max_profit, jobs[i][1])
+                i += 1
+            total_profit += max_profit
+        return total_profit
