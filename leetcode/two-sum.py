66b736f2851e3f8298b0df5fba119189990bf4ee
Sameer Passi <sameerbpassi@gmail.com>
2025-10-29 18:59:31 -0400

    Update solution for two-sum

--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -2,8 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashMap = {}
 
-        for i, num in enumerate(nums):
-            difference = target - num
-            if difference in hashMap:
-                return [hashMap[difference], i]
-            hashMap[num] = i
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hashMap:
+                return [hashMap[diff], i]
+            hashMap[n] = i
